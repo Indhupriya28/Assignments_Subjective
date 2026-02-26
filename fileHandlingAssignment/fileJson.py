67a49e8ed96d6a3234bbdd73d99dce1526aac5be
indhupriya <indhupriyadeisigan@gmail.com>
2026-02-26 23:59:33 +0530

    File_json handling

--- fileHandlingAssignment/fileJson.py
+++ fileHandlingAssignment/fileJson.py
@@ -1,9 +1,9 @@
 import json
 
-#open the file in the read mode using context manager.
-#Hence no need to close the file- this will be handled automatically.
-#Using json module from the python- load() help to read the json from the File and stored that data as python object (list)
-#To calculate the number of books len() is used
+'''open the file in the read mode using context manager.
+Hence no need to close the file- this will be handled automatically.
+Using json module from the python- load() help to read the json from the File and stored that data as python object (list)
+To calculate the number of books len() is used'''
 def readJson():
     with open("inventory.json","r") as file: 
         inventory = json.load(file)
@@ -17,20 +17,24 @@ def saveJson():
     # Open file with read mode and load the json data into python object
     with open ("inventory.json","r") as file:
         inventory = json.load(file)
-    #Open file in Write mode , adding the new data into the python list using append()
-    #using dump() python data is converted to Json and placed in the file.
-    #indent is used for formatting purpose (indentation space)
+    '''Open file in Write mode , adding the new data into the python list using append()
+    using dump() python data is converted to Json and placed in the file.
+    indent is used for formatting purpose (indentation space)'''
     with open("inventory.json","w") as file:
         inventory.append(new_book)
         json.dump(inventory,file,indent=4)
 
 
+'''NOTE: On using "r+" in above function, using dump() willnot overwrite the existing data.
+Inorder to do that, need to use move the cursor to the position 0 using "seek(0)". 
+then dump() the data and delete the old data using file.truncate() '''
+
 
 
 
 def printJson():
-    #read the data from the json file . Used for loop to iterate into each data.
-    #using print(f ) datas are printed in a expected format
+    '''read the data from the json file . Used for loop to iterate into each data.
+    using print(f ) datas are printed in a expected format'''
     with open ("inventory.json","r") as file:
         inventory = json.load(file)
         for data in inventory:
